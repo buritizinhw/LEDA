e359e218559bc6d25839fa6e94b0233191429ede
Lívia Buriti <92129840+buritizinhw@users.noreply.github.com>
2023-08-13 22:21:52 -0300

    Sorting simples

--- R01-02-Rot-SimpleSorting-Bidirectional-Bubble-environment/src/main/java/sorting/simpleSorting/InsertionSort.java
+++ R01-02-Rot-SimpleSorting-Bidirectional-Bubble-environment/src/main/java/sorting/simpleSorting/InsertionSort.java
@@ -10,9 +10,23 @@ import sorting.AbstractSorting;
  */
 public class InsertionSort<T extends Comparable<T>> extends AbstractSorting<T> {
 
-	@Override
-	public void sort(T[] array, int leftIndex, int rightIndex) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not Implemented yet!");
-	}
+    @Override
+    public void sort(T[] array, int leftIndex, int rightIndex) {
+        if (array == null || leftIndex < 0 || rightIndex >= array.length || leftIndex >= rightIndex) {
+            return;
+        }
+
+        for (int i = leftIndex + 1; i <= rightIndex; i++) {
+            T key = array[i];
+            int j = i - 1;
+
+            // Move os elementos que são maiores que a chave para uma posição à frente de sua posição atual
+            while (j >= leftIndex && array[j].compareTo(key) > 0) {
+                array[j + 1] = array[j];
+                j--;
+            }
+            array[j + 1] = key;
+        }
+    }
 }
+
