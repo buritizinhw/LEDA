abdb71020aba8585a341b3ef87b3da2faacb9547
Lívia Buriti <92129840+buritizinhw@users.noreply.github.com>
2023-09-01 11:19:41 -0300

    roteiro 06

--- R06-02-Rot-BinarySearch-Raiz-environment/src/main/java/problems/RaizImpl.java
+++ R06-02-Rot-BinarySearch-Raiz-environment/src/main/java/problems/RaizImpl.java
@@ -9,9 +9,35 @@ package problems;
  */
 public class RaizImpl implements Raiz {
 
-	public double raiz(int numero, int raiz, double erro){
-		//TODO Implement your code here
-		throw new UnsupportedOperationException("Not implemented yet!");
-	}
+	public double raiz(int numero, int raiz, double erro) {
+		double low = 0;
+        double high = numero;
+        return raizRecursivo(numero, raiz, erro, low, high);
+    }
+
+    private double raizRecursivo(int numero, int raiz, double erro, double low, double high) {
+        double mid = (low + high) / 2;
+        double approx = approximate(mid, raiz);
+
+        if (Math.abs(approx - numero) <= erro) {
+            return mid;
+        }
+
+        
+        if (approx < numero) {
+            return raizRecursivo(numero, raiz, erro, mid, high);
+        } else {
+            return raizRecursivo(numero, raiz, erro, low, mid);
+        }
+    }
+
+
+    private double approximate(double x, int n) {
+        double result = 1.0;
+        for (int i = 0; i < n; i++) {
+            result *= x;
+        }
+        return result;
+    }
 
 }
